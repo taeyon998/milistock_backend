99080f0574621f2c556d567baa119adcadf26f8e
Taeyon Kim <taeyon998@gmail.com>
2023-07-27 14:00:43 +0000

    ddsaf

---
+++ milistock/src/main/java/com/milistock/milistock/MilistockApplication.java
@@ -0,0 +1,13 @@
+package com.milistock.milistock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilistockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilistockApplication.class, args);
+	}
+
+}
---
+++ milistock/src/test/java/com/milistock/milistock/MilistockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milistock.milistock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilistockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
