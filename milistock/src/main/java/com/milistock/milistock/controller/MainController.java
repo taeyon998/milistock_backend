88a16a285d593ec29850fcf4b33f22b3d0d9d95b
Taeyon Kim <taeyon998@gmail.com>
2023-08-01 12:20:22 +0000

    d

--- milistock/src/main/java/com/milistock/milistock/controller/MainController.java
+++ milistock/src/main/java/com/milistock/milistock/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.milistock.milistock.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(originPatterns = "https://3000-taeyon998-milistockfron-cnpzg1ltsid.ws-us102.gitpod.io/")
 @RestController
 @RequestMapping("/")
 public class MainController {
