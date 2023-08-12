6bf3958e71a67297f6e1ec619d85a561328340e9
Taeyon Kim <taeyon998@gmail.com>
2023-08-12 10:23:19 +0000

    signup AuthService

--- milistock/src/main/java/com/milistock/milistock/controller/AuthController.java
+++ milistock/src/main/java/com/milistock/milistock/controller/AuthController.java
@@ -1,26 +1,27 @@
 package com.milistock.milistock.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.milistock.milistock.dto.ResponseDto;
-import com.milistock.milistock.dto.SigninResponseDto;
 import com.milistock.milistock.dto.SignUpDto;
+import com.milistock.milistock.service.AuthService;
 
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    @GetMapping("")
-    public String hello(){
-        return "Connection Successful";
-    }
+    
+    @Autowired AuthService authService;
+
     @PostMapping("/signUp")
-    public ResponseDto<SigninResponseDto> signUp(@RequestBody SignUpDto requestBody) {
-        System.out.println(requestBody.toString());
-        return null;
+    public ResponseDto<?> signUp(@RequestBody SignUpDto requestBody) {
+        System.out.println(requestBody.toString()); // optional, delete later
+
+        ResponseDto<?> result = authService.signUp(requestBody);
+        return result;
     }
 }
--- milistock/src/main/java/com/milistock/milistock/service/AuthService.java
+++ milistock/src/main/java/com/milistock/milistock/service/AuthService.java
@@ -1,17 +1,40 @@
 package com.milistock.milistock.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.milistock.milistock.dto.ResponseDto;
 import com.milistock.milistock.dto.SignUpDto;
+import com.milistock.milistock.entity.UserEntity;
+import com.milistock.milistock.repository.UserRepository;
 
 @Service
 public class AuthService {
+
+    @Autowired UserRepository userRepository;
+
     public ResponseDto<?> signUp(SignUpDto dto) {
+        String userEmail = dto.getUserEmail();
         String userpassword = dto.getUserPassword();
         String userpasswordCheck = dto.getUserPasswordCheck();
 
+        // email 중복 확인
+        userRepository.existsById(userEmail);
+        
+        // password check
         if (!userpassword.equals(userpasswordCheck))
-            return ResponseDto.setFailed("Pswd does not match!")
+            return ResponseDto.setFailed("Pswd does not match!");
+
+        // dto를 통해 프론트에서 받아온 데이터로 user object 만듬
+        UserEntity userEntity = new UserEntity(dto);
+
+        // Repo를 통해 DB에 저장
+        try {
+            userRepository.save(userEntity);
+        } catch (Exception e) {
+            return ResponseDto.setFailed("Data Base Error!");
+        }
+
+        return ResponseDto.setSuccess("Sign Up Success!", null);
     }
 }
