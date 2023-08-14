32376f8eb5ec1ed7fe248de9c1a22929ef865c64
Taeyon Kim <taeyon998@gmail.com>
2023-08-14 15:10:01 +0000

    d

--- milistock/src/main/java/com/milistock/milistock/security/TokenProvider.java
+++ milistock/src/main/java/com/milistock/milistock/security/TokenProvider.java
@@ -1,6 +1,14 @@
 package com.milistock.milistock.security;
 
+import java.util.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class TokenProvider {
@@ -9,6 +17,22 @@ public class TokenProvider {
 
     // JWT 생성하는 메서드
     public String create (String userEmail) {
-        
+        // 만료날짜 -> 현재 날짜 + 1시간 설정
+        Date exprTime = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+
+        // JWT를 생성
+        return Jwts.builder()
+            .signWith(SignatureAlgorithm.HS512, SECURITY_KEY)
+            .setSubject(userEmail).setIssuedAt(new Date()).setExpiration(exprTime)
+            // 생성
+            .compact();
+    }
+
+    // JWT 검증
+    public String validate (String token) {
+        // token을 디코딩
+        Claims claims = Jwts.parser().setSigningKey(SECURITY_KEY).parseClaimsJws(token).getBody();
+        // payload에서 제목 가져옴
+        return claims.getSubject();
     }
 }
